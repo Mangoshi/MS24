4ce505193dde837129d75a7ca499ddc249f44a66
mangoshi <soggymoose@gmail.com>
2023-02-02 16:22:32 +0000

    Tried making webaudio-controls elements responsive- failed.

--- src/components/OscillatorGroup.js
+++ src/components/OscillatorGroup.js
@@ -1,13 +1,29 @@
+import {useEffect, useState} from "react";
 
+const OscillatorGroup = ({x, y}) => {
 
-const OscillatorGroup = () => {
+	const [diameter, setDiameter] = useState(50);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDiameter((d) => {
+				if (d >= 200) {
+					clearInterval(interval);
+				}
+				document.querySelector("#k1").diameter = d;
+				return d + 20;
+			});
+		}, 1000);
+	}, []);
+
+	console.log(diameter)
+
 	return (
-		<div>
-			<h1>Oscillator</h1>
-			<webaudio-knob id="knob-2" min="0" max="100" colors="#FF6188;#2C292D;#D9D9D9"></webaudio-knob>
-			<webaudio-knob id="knob-2" min="0" max="100" colors="#A9DC76;#2C292D;#D9D9D9"></webaudio-knob>
-			<webaudio-knob id="knob-2" min="0" max="100" colors="#FFD866;#2C292D;#D9D9D9"></webaudio-knob>
-			<webaudio-slider colors="#78DCE8;#2C292D;#D9D9D9" direction="vert"></webaudio-slider>
+		<div style={{top: y, left: x, position: 'absolute'}}>
+			<webaudio-knob diameter={diameter} id="k1" min="0" max="100" colors="#FF6188;#2C292D;#D9D9D9"></webaudio-knob>
+			<webaudio-knob id="k2" min="0" max="100" colors="#A9DC76;#2C292D;#D9D9D9"></webaudio-knob>
+			<webaudio-knob id="k3" min="0" max="100" colors="#FFD866;#2C292D;#D9D9D9"></webaudio-knob>
+			<webaudio-slider id="s1" colors="#78DCE8;#2C292D;#D9D9D9" direction="vert"></webaudio-slider>
 		</div>
 	);
 }
--- src/pages/Synth.js
+++ src/pages/Synth.js
@@ -2,7 +2,8 @@ import * as Tone from 'tone'
 
 import OscillatorGroup from '../components/OscillatorGroup'
 
-import frame from './assets/Frame.svg'
+import frame_svg from './assets/Frame.svg'
+import {useRef} from "react";
 
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
@@ -12,18 +13,33 @@ function playNote() {
 	synth.triggerAttackRelease("C4", "8n");
 }
 
-const frameStyling = {
-	width: '100%'
+const containerStyling = {
+	aspectRatio: '16/9',
+	position: 'relative',
+}
+
+const frameStyling1 = {
+	width: '100%',
+	height: '100%',
+	backgroundImage: `url(${frame_svg})`,
+	backgroundSize: 'contain',
+	backgroundRepeat: 'no-repeat',
+	position: 'relative',
+}
+
+const frameStyling2 = {
+	width: '100%',
+	height: '100%',
+	backgroundColor: 'grey'
 }
 
 const Synth = () => {
+	const parentRef = useRef(null);
 	return (
-		<div>
-			<h1>Inika</h1>
-			<h2>Inconsolata</h2>
-			<button onClick={playNote}>Play Note</button>
-			<img src={frame} alt='Frame SVG' style={frameStyling}/>
-			<OscillatorGroup/>
+		<div style={containerStyling}>
+			<div ref={parentRef} style={frameStyling2}>
+					<OscillatorGroup parentRef={parentRef} x={100} y={200} heightPercent={5} widthPercent={5}/>
+			</div>
 		</div>
 	);
 }

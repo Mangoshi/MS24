72f2991abcb8ae126cbdd0d8de0cae8bff978666
mangoshi <soggymoose@gmail.com>
2023-04-13 18:16:44 +0100

    MIDI access class added
    - This will allow listening to MIDI controllers
    - Will try to connect it to Tone in future commits

--- src/synth.js
+++ src/synth.js
@@ -12,6 +12,57 @@ await register(await connect())
 	.then(() => {console.log("MediaRecorder WAV encoder registered")})
 	.catch((err) => {console.error(err)})
 
+class MIDIAccess {
+	constructor(args = {}) {
+		this.onDeviceInput = args.onDeviceInput || console.log;
+	}
+
+	start() {
+		return new Promise((resolve, reject) => {
+			this._requestAccess().then(access => {
+				this.initialize(access);
+				resolve();
+			}).catch(() => reject('Something went wrong.'));
+		});
+	}
+
+	initialize(access) {
+		const devices = access.inputs.values();
+		for (let device of devices) this.initializeDevice(device);
+	}
+
+	initializeDevice(device) {
+		device.onmidimessage = this.onMessage.bind(this);
+	}
+
+	onMessage(message) {
+		let [_, input, value] = message.data;
+		this.onDeviceInput({ input, value });
+	}
+
+	_requestAccess() {
+		return new Promise((resolve, reject) => {
+			if (navigator.requestMIDIAccess)
+				navigator.requestMIDIAccess()
+					.then(resolve)
+					.catch(reject);
+			else reject();
+		});
+	}
+}
+
+const midi = new MIDIAccess({ onDeviceInput })
+
+midi.start().then(() => {
+	console.log("MIDI Access started")
+}).catch((err) => {
+	console.error(err)
+})
+
+function onDeviceInput({input, value}) {
+	console.log('onDeviceInput', input, value)
+}
+
 // import resolveConfig from 'tailwindcss/resolveConfig'
 // import tailwindConfig from './tailwind.config.cjs'
 
